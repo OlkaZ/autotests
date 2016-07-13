c065a7fc42aa9e6a07c3d29bd5d5966f7cebd01e
Olga Khoroshok <olga.khoroshok@gmail.com>
2016-07-14 00:42:15 +0300

    SRC commite q

---
+++ src/com/OlkaZ/utils/convertors/Executor.java
@@ -0,0 +1,10 @@
+package com.OlkaZ.utils.convertors;
+
+public class Executor {
+    public static void main (String args[]) {
+        PrimitiveConvertor primitiveConvertor = new PrimitiveConvertor();
+        primitiveConvertor.floatToChar();
+        primitiveConvertor.intToChar();
+        primitiveConvertor.charToInt();
+    }
+}
---
+++ src/com/OlkaZ/utils/convertors/PrimitiveConvertor.java
@@ -0,0 +1,19 @@
+package com.OlkaZ.utils.convertors;
+
+
+import java.awt.*;
+
+public class PrimitiveConvertor {
+    float float1 = 5.6F;
+    int int1 = 16;
+    char char1 = 'u';
+    public void floatToChar() {
+        System.out.println("Input float value is " + float1 + ". Output char value is " + char1);
+    }
+    public void intToChar(){
+        System.out.println("Input int value is " + int1 + ". Output char value is " + char1);
+    }
+    public void charToInt(){
+        System.out.println("Input char value is " + char1 + ". Output int value is " + int1);
+    }
+}
